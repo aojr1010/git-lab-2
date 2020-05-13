b19bfbc4fcfb4382e0bf7bb6fe44f4947091508b
Anthony Owusu-Ansah <ao264517@ohio.edu>
2020-05-13 16:49:55 -0400

    First upload

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Anthony Owusu-Ansah
+ *        Date: 5/13/2020
+ * Description: First test program. 
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!" << endl;
+    return 0;
+}// main
